76d311432435d3427258f1099108ec2fc9b13931
Giygas <gustavorossich@gmail.com>
2024-06-02 15:54:43 +0200

    Base delete button done

---
+++ components/DeleteBookButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import { Button } from "@/components/ui/button"
+
+  // The component receives the book id to delete
+  const props = defineProps<{
+    bookId: number
+  }>()
+
+  function handleDelete() {
+    console.log(props.bookId)
+  }
+</script>
+
+<template>
+  <Button variant="destructive" @click="handleDelete()"> Delete Book </Button>
+</template>
--- server/api/books/[id].delete.ts
+++ server/api/books/[id].delete.ts
@@ -1,1 +1,31 @@
 // TODO: delete book by id
+import { eq } from "drizzle-orm"
+import { db } from "~/server/database/db"
+import { books, selectBookSchema } from "~/server/database/schema"
+
+export default defineEventHandler(async (event) => {
+  const bookId = Number(getRouterParam(event, "id"))
+
+  const deletedBook = await db
+    .delete(books)
+    .where(eq(books.id, bookId))
+    .returning()
+
+  if (deletedBook) {
+    const parsedBook = selectBookSchema.parse(deletedBook)
+
+    setResponseStatus(event, 200)
+    return {
+      success: true,
+      deletedBook: parsedBook,
+      message: `Book ${parsedBook.title} deleted from the database`,
+    }
+  } else {
+    setResponseStatus(event, 500)
+    return {
+      success: false,
+      deletedBook: undefined,
+      message: "Error deleting book from the database",
+    }
+  }
+})
