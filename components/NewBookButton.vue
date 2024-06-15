f999f54d7af5471bc27f0b61a59e179e70dddd48
Giygas <gustavorossich@gmail.com>
2024-06-16 00:58:03 +0200

    Dialog now working with edit mode or new book

--- components/NewBookButton.vue
+++ components/NewBookButton.vue
@@ -22,10 +22,16 @@
       <Button v-if="isBookEdit" variant="outline"> Edit Book </Button>
     </DialogTrigger>
     <DialogContent class="w-full">
-      <DialogHeader>
+      <DialogHeader v-if="isBookEdit">
         <DialogTitle>Edit Book</DialogTitle>
         <DialogDescription>
           You can edit the book here. Click submit when you're done.
+        </DialogDescription>
+      </DialogHeader>
+      <DialogHeader v-if="!isBookEdit">
+        <DialogTitle>New Book</DialogTitle>
+        <DialogDescription>
+          You can add a new book here. Click submit when you're done.
         </DialogDescription>
       </DialogHeader>
       <NewBookForm :book-id="id" />
